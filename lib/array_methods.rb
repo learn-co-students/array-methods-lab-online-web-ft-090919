0dc8e5ef333f668c1d1251f5bb66b163e9e79397
Craig Christopher Messi <craig.messi@gmail.com>
2019-09-11 03:23:51 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,23 +1,23 @@
 def using_include(array, element)
-  ["wow", "I", "am", "really", "learning", "arrays!"].include?(element)
+  array.include?(element)
 end
 
 def using_sort(array)
-  ["wow", "I", "am", "really", "learning", "arrays!"].sort
+  array.sort
 end
 
 def using_reverse(array)
-  ["wow", "I", "am", "really", "learning", "arrays!"].reverse
+  array.reverse
 end
 
 def using_first(array)
-  ["wow", "I", "am", "really", "learning", "arrays!"].first
+  array.first
 end
 
 def using_last(array)
-  ["wow", "I", "am", "really", "learning", "arrays!"].last
+  array.last
 end
 
 def using_size(array)
-  ["wow", "I", "am", "really", "learning", "arrays!"].size
+  array.size
 end
